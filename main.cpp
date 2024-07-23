01430893fba18ddf9171909164fd1a54a9d2774c
上野太己 <k023g0051@g.neec.ac.jp>
2024-07-23 11:19:39 +0900

    objファイル読み込み

--- main.cpp
+++ main.cpp
@@ -244,22 +244,22 @@ void UploadTextureData(ID3D12Resource* texture, const DirectX::ScratchImage& mip
 }
 
 ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename) {
-	//1.中で必要となる変数の宣言
+	// 1.中で必要となる変数の宣言
 	ModelData modelData;  //構築するModelData
 	std::vector<Vector4> positions;  //位置
 	std::vector<Vector3> normals;  //法線
 	std::vector<Vector2> texcoords;  //テクスチャ座標
 	std::string line;  //ファイルから読んだ1行を格納するもの
-	//2.ファイルを開く
+	// 2.ファイルを開く
 	std::ifstream file(directoryPath + "/" + filename);  //ファイルを開く
 	assert(file.is_open());  //とりあえず開けなかったら止める
-	//3.実際にファイルを読み、ModelDataを構築していく
+	// 3.実際にファイルを読み、ModelDataを構築していく
 	while (std::getline(file, line)) {
 		std::string identifier;
 		std::istringstream s(line);
 		s >> identifier;  //先頭の識別子を読む
 
-		//identifierに応じた処理
+		// identifierに応じた処理
 		if (identifier == "v") {
 			Vector4 position = {};
 			s >> position.x >> position.y >> position.z;
@@ -277,7 +277,7 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 			normals.push_back(normal);
 		}
 		else if (identifier == "f") {
-			VertexData triangle[3] = {};
+			VertexData triangle[3];
 			//面は三角形限定。
 			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
 				std::string vertexDefinition;
@@ -290,21 +290,22 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 					std::getline(v, index, '/');  //区切りでインデックスを読んでいく
 					elementIndices[element] = std::stoi(index);
 				}
-				//要素へのIndexから、実際の要素の値を取得して、頂点を構築する
+				//要素へのIndexから、実際の要素を値を取得して、頂点を構築する
 				Vector4 position = positions[elementIndices[0] - 1];
 				Vector2 texcoord = texcoords[elementIndices[1] - 1];
 				Vector3 normal = normals[elementIndices[2] - 1];
 				position.x *= -1.0f;
+				texcoord.y = 1.0f - texcoord.y;
 				normal.x *= -1.0f;
 				triangle[faceVertex] = { position, texcoord, normal };
 			}
-			//頂点を逆順で登録することで、回り順を逆にする
+			//頂点を逆順で登録することで、周り順を逆にする
 			modelData.vertices.push_back(triangle[2]);
 			modelData.vertices.push_back(triangle[1]);
 			modelData.vertices.push_back(triangle[0]);
 		}
 	}
-	//4.ModelDataを返す
+	// 4.ModelDataを返す
 	return modelData;
 }
 
