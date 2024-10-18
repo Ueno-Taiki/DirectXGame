ca166cedc04843e14aa03907968c4e57c97ca775
上野太己 <k023g0051@g.neec.ac.jp>
2024-10-18 12:14:33 +0900

    更新

--- main.cpp
+++ main.cpp
@@ -752,11 +752,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
 
 	//Shaderをコンパイルする
-	IDxcBlob* vertexShaderBlob = CompileShader(L"resources/shaders/Object3D.VS.hlsl",
+	IDxcBlob* vertexShaderBlob = CompileShader(L"Particle.VS.hlsl",
 		L"vs_6_0", dxcUtils, dxcCompiler, includeHandler);
 	assert(vertexShaderBlob != nullptr);
 
-	IDxcBlob* pixelShaderBlob = CompileShader(L"resources/shaders/Object3D.PS.hlsl",
+	IDxcBlob* pixelShaderBlob = CompileShader(L"Particle.PS.hlsl",
 		L"ps_6_0", dxcUtils, dxcCompiler, includeHandler);
 	assert(pixelShaderBlob != nullptr);
 
@@ -1078,11 +1078,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			//マテリアルCBufferの場所を設定
 			commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
 			//SRVのDescriptorTableの先頭を設定。
-			commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
-			//instancing用のDataを読むためにstructuredBufferのSRVを設定する
-			commandList->SetGraphicsRootConstantBufferView(1, instancingSrvHandleGPU);
+			commandList->SetGraphicsRootDescriptorTable(2, instancingSrvHandleGPU);
 			//描画
 			commandList->DrawInstanced(UINT(modelData.vertices.size()), kNumInstance, 0, 0);
+			/*
 			//Spriteの描画
 			//commandList->IASetVertexBuffers(0, 1, &vertexBufferViewSprite);  //VBVを設定
 			commandList->IASetIndexBuffer(&indexBufferViewSprite);  //IBVを設定
@@ -1091,6 +1090,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			//描画
 			commandList->DrawInstanced(6, 1, 0, 0);
 			commandList->DrawIndexedInstanced(6, 1, 0, 0, 0);
+			*/
 			ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), commandList);
 			//状態を遷移
 			barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
