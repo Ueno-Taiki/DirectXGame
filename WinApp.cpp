6937f7f73b4883ce37a461097b4bcb67395f025d
上野太己 <k023g0051@g.neec.ac.jp>
2024-11-18 14:29:07 +0900

    03_02_完了

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,7 +1,7 @@
 #include "WinApp.h"
 
 //ウインドウブロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+LRESULT CALLBACK WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 	if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam)) {
 		return true;
